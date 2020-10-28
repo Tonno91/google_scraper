1e411df1d18f03c51ff6114ad092bae98631ed94
Luca Tonini <luca.tonini@lamborghini.com>
2020-10-28 21:33:08 +0100

    Function comment add

--- google_scraper/input_check.py
+++ google_scraper/input_check.py
@@ -2,6 +2,9 @@ from tkinter import messagebox
 
 
 def gui_input(website, rep_time, rep_limit, result_num, key_word):
+    """This function check the feasibility of the input variables
+    and show an error message in case something is wrong"""
+
     if website != "Google":
         messagebox.showerror("Error", "The website allowed is only Google")
         return 0
